cf27f77fb5c709deaad3cc18aa49a1b7a0178d8e
Valeria Otchenash <kratorchestra@gmail.com>
2023-06-21 17:23:26 +0300

    adds task#3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,27 @@
 import { Profile } from '../components/Profile/Profile';
 import user from '../components/data/user.json';
-import { Statistics} from './Statistics/Statistics';
+import { Statistics } from './Statistics/Statistics';
 import data from '../components/data/data.json';
+import { FriendList } from '../components/FriendList/FriendList';
+import friends from '../components/data/friends.json';
+import transactions from '../components/data/transactions.json';
 
 const App = () => {
   return (
     <div>
-  <Profile
-    avatar={user.avatar}
-    username={user.username}
-    tag={user.tag}
-    location={user.location}
-    stats={user.stats}
+      <Profile
+        avatar={user.avatar}
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        stats={user.stats}
       />
-      
-    <Statistics title="Upload stats" stats={data} />
-   
-      
-      
 
+      <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friends} />
     </div>
-  )   
-}
+  );
+};
 
 export default App;
-
---
+++ src/components/FriendList/FriendList.js
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+import css from './FriensList.module.css';
+
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className={css.friendList}>
+      {friends.map(({ id, avatar, name, isOnline }) => (
+        <FriendListItem
+          key={id}
+          avatar={avatar}
+          name={name}
+          isOnline={isOnline}
+        />
+      ))}
+    </ul>
+  );
+};
+
+FriendList.propType = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    })
+  ),
+};
